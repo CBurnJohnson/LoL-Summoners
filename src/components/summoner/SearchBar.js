97e196a1e89affb38e047b71dd8c6835c4fe65ce
Cole Johnson <Colejohnson147@gmail.com>
2019-11-21 20:24:45 -0800

    clean up

--- src/components/summoner/SearchBar.js
+++ src/components/summoner/SearchBar.js
@@ -2,8 +2,6 @@ import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import SummonerContext from '../../context/summoner/summonerContext';
 
-import Home from '../pages/Home';
-
 const SearchBar = () => {
     const summonerContext = useContext(SummonerContext);
 
