75a2912458e3b18f127cfbaaab2a87f43c43afbe
Cole Johnson <Colejohnson147@gmail.com>
2019-11-30 16:50:03 -0800

    match item styling/logic

--- src/components/summoner/Match.js
+++ src/components/summoner/Match.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
-const Match = () => {
+const Match = props => {
+    const {
+        participantIdentities,
+        gameType,
+        gameMode,
+        gameDuration
+    } = props.match;
+
+    const leftTeam = participantIdentities.filter(participant => {
+        return participant.participantId <= 5;
+    });
+
+    const rightTeam = participantIdentities.filter(participant => {
+        return participant.participantId > 5;
+    });
+
     return (
-        <div>
-            <h1>hello</h1>
+        <div className='match-item'>
+            <div className='match-info'>
+                <div className='info'>
+                    <div className='game-type'>{gameType}</div>
+                    <div className='game-mode'>{gameMode}</div>
+                    <div className='game-duration'>{gameDuration / 60}</div>
+                </div>
+                <div className='outcome'>
+                    <div>VICTORY</div>
+                </div>
+            </div>
+            <div className='players'>
+                <div className='left-team'>
+                    {leftTeam.map(player => (
+                        <div key={player.player.summonerName}>
+                            {player.player.summonerName}
+                        </div>
+                    ))}
+                </div>
+                <div className='right-team'>
+                    {rightTeam.map(player => (
+                        <div key={player.player.summonerName}>
+                            {player.player.summonerName}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
